05f22afee4d6e12fc695134e14699ecbfd4f8e4a
Jayden Quallio <52089788+JaydenQuallio@users.noreply.github.com>
2023-05-04 15:05:29 -0600

    Done
    :+1:

--- Donuts/Donuts/ContentView.swift
+++ Donuts/Donuts/ContentView.swift
@@ -1,43 +1,56 @@
 import SwiftUI
 import MessageUI
 import RealityKit
+import ARKit
 
 struct ContentView: View {
-    @State private var message = "Order for Jayden: "
+    @State private var message = "Order for Jayden:"
     @State private var isShowingMessageComposer = false
-    @State private var showCount: Int = 0
-
+    @State private var errorText: String = ""
+    @State private var errorBool: Bool = false
+    @State var showCount: Int = -1
     @State var showButtons = true
-    @State var button = 0;
-    
+    @State var button = 0
     
     @State var pressedButtons: [String] = []
+    @State var finalOrder: [String] = []
     
     var body: some View {
-        if(showCount < 1){
+        if(showCount == 0){
             if (showButtons) {
                 VStack {
-                    Button("Drinks") {
-                        button = 1
-                        showButtons = false
+                    Spacer()
+                    Image("Guru")
+                        .resizable()
+                        .frame(width: 200, height: 200)
+                    Spacer()
+                    HStack{
+                        Button("Drinks") {
+                            button = 1
+                            showButtons = false
+                        }
+                        .padding()
+                        .background(Color.blue)
+                        .foregroundColor(.white)
+                        .cornerRadius(10)
+                        .padding()
+                        
+                        Button("Food") {
+                            button = 2
+                            showButtons = false
+                        }
+                        .padding()
+                        .background(Color.blue)
+                        .foregroundColor(.white)
+                        .cornerRadius(10)
+                        .padding()
                     }
-                    .padding()
-                    .background(Color.blue)
-                    .foregroundColor(.white)
-                    .cornerRadius(10)
-                    .padding()
-                    
-                    Button("Food") {
-                        button = 2
-                        showButtons = false
-                    }
-                    .padding()
-                    .background(Color.blue)
-                    .foregroundColor(.white)
-                    .cornerRadius(10)
-                    .padding()
-                    
                     Button("YOUR ORDER") {
+                        finalOrder = pressedButtons.reduce(into: [:]) { counts, element in
+                            counts[element, default: 0] += 1
+                        }
+                        .map { "\($0.key) x \($0.value)" }
+                        
                         button = 3
                         showButtons = false
                     }
@@ -47,7 +60,8 @@ struct ContentView: View {
                     .cornerRadius(10)
                     .padding()
                     
-                    
+                    Spacer()
+                    Spacer()
                 }
             }
             else {
@@ -56,65 +70,93 @@ struct ContentView: View {
                     switch (button) {
                         
                     case 1:
-                        Button("Coffee") {
+                        HStack{
+                            Button("Coffee ☕️") {
+                                
+                                
+                                pressedButtons.append("Coffee ☕️")
+                                
+                                
+                                
+                                showButtons = true
+                            }
+                            .padding()
+                            .background(Color.blue)
+                            .foregroundColor(.white)
+                            .cornerRadius(10)
+                            .padding()
                             
+                            Button("Tea 🫖") {
+                                
+                                pressedButtons.append("Tea 🫖")
+                                
+                                showButtons = true
+                            }
+                            .padding()
+                            .background(Color.blue)
+                            .foregroundColor(.white)
+                            .cornerRadius(10)
+                            .padding()
+                        }
+                        HStack{
+                            Button("Soda 🥤") {
+                                
+                                pressedButtons.append("Soda 🥤")
+                                
+                                showButtons = true
+                            }
+                            .padding()
+                            .background(Color.blue)
+                            .foregroundColor(.white)
+                            .cornerRadius(10)
+                            .padding()
                             
-                            pressedButtons.append("Coffee")
-                            
-                            
-                            
+                            Button("Water 💧") {
+                                
+                                pressedButtons.append("Water 💧")
+                                
+                                showButtons = true
+                            }
+                            .padding()
+                            .background(Color.blue)
+                            .foregroundColor(.white)
+                            .cornerRadius(10)
+                            .padding()
+                        }
+                        Button("Back"){
                             showButtons = true
                         }
                         .padding()
-                        .background(Color.blue)
-                        .foregroundColor(.white)
+                        .background(.gray)
+                        .foregroundColor(.primary)
                         .cornerRadius(10)
                         .padding()
-                        
-                        Button("Tea") {
-                            
-                            pressedButtons.append("Tea")
-                            
-                            showButtons = true
-                        }
-                        .padding()
-                        .background(Color.blue)
-                        .foregroundColor(.white)
-                        .cornerRadius(10)
-                        .padding()
-                        
-                        Button("Soda") {
-                            
-                            pressedButtons.append("Soda")
-                            
-                            showButtons = true
-                        }
-                        .padding()
-                        .background(Color.blue)
-                        .foregroundColor(.white)
-                        .cornerRadius(10)
-                        .padding()
-                        
-                        Button("Water") {
-                            
-                            pressedButtons.append("Water")
-                            
-                            showButtons = true
-                        }
-                        .padding()
-                        .background(Color.blue)
-                        .foregroundColor(.white)
-                        .cornerRadius(10)
-                        .padding()
-                        
-                        
-                        
-                        
                         
                     case 2:
-                        Button("Donuts") {
+                        HStack{
+                            Button("Donut 🍩") {
+                                
+                                pressedButtons.append("Donut 🍩")
+                                showButtons = true
+                            }
+                            .padding()
+                            .background(Color.blue)
+                            .foregroundColor(.white)
+                            .cornerRadius(10)
+                            .padding()
                             
-                            pressedButtons.append("Donuts")
+                            Button("Sandwich 🥪") {
+                                pressedButtons.append("Sandwich 🥪")
+                                showButtons = true
+                            }
+                            .padding()
+                            .background(Color.blue)
+                            .foregroundColor(.white)
+                            .cornerRadius(10)
+                            .padding()
+                        }
+                        Button("Toast 🍞") {
+                            pressedButtons.append("Toast 🍞")
                             showButtons = true
                         }
                         .padding()
@@ -123,47 +165,38 @@ struct ContentView: View {
                         .cornerRadius(10)
                         .padding()
                         
-                        Button("Sandwiches") {
-                            pressedButtons.append("Sandwiches")
+                        Button("Back"){
                             showButtons = true
                         }
                         .padding()
-                        .background(Color.blue)
-                        .foregroundColor(.white)
+                        .background(.gray)
+                        .foregroundColor(.primary)
                         .cornerRadius(10)
                         .padding()
-                        
-                        Button("Toast") {
-                            pressedButtons.append("Toast")
-                            showButtons = true
-                        }
-                        .padding()
-                        .background(Color.blue)
-                        .foregroundColor(.white)
-                        .cornerRadius(10)
-                        .padding()
-                        
-                        
                         
                     case 3:
-                        Text("Your order:")
+                        Text(errorText)
+                            .padding()
+                            .background(Color.red)
+                            .foregroundColor(.white)
+                            .font(.headline)
+                            .opacity(errorBool ? 1 : 0)
+                        
+                        Text("Your Order:")
                             .font(.title)
-                        ForEach(pressedButtons, id: \.self) { button in
+                        ForEach(finalOrder, id: \.self) { button in
                             Text(button)
                                 .font(.headline)
                         }
-                        Button("Back") {
-                            showButtons = true
-                            message = "Order for Jayden"
-                        }
-                        .padding()
-                        .background(Color.orange)
-                        .foregroundColor(.white)
-                        .cornerRadius(10)
-                        .padding()
                         
                         Button("Place Order"){
-                            isShowingMessageComposer = true
+                            if(pressedButtons.count > 0){
+                                isShowingMessageComposer = true
+                            }
+                            else{
+                                errorBool = true
+                                errorText = "Please Add Something to Your Order!"
+                            }
                         }
                             .padding()
                             .background(Color.orange)
@@ -171,15 +204,26 @@ struct ContentView: View {
                             .cornerRadius(10)
                             .padding()
                         
+                        Button("Back") {
+                            showButtons = true
+                            errorBool = false
+                            message = "Order for Jayden:"
+                        }
+                        .padding()
+                        .background(.gray)
+                        .foregroundColor(.primary)
+                        .cornerRadius(10)
+                        .padding()
+                        
                     default:
                         //print("yeah")
-                        Text("Safety")
+                        Text("Restart App")
                         //showButtons = true
                         
                     }
                 }
                 .sheet(isPresented: $isShowingMessageComposer) {
-                    MessageComposeView(message: AddOrder(pressedButtons: pressedButtons, message: message), recipient: "2083921227", isShowingMessageComposer: $isShowingMessageComposer, showCount: $showCount)
+                    MessageComposeView(message: AddOrder(pressedButtons: finalOrder, message: message), recipient: "2083921227", isShowingMessageComposer: $isShowingMessageComposer, showCount: $showCount)
                 }
             }
         }
@@ -191,8 +235,9 @@ struct ContentView: View {
                     .font(.title2)
                 
                 Button("Next") {
-                    isShowingMessageComposer = true
+                    pressedButtons = [String]()
                     showCount = 2
+                    showButtons = true
                 }
                 .padding()
                 .foregroundColor(.white)
@@ -202,7 +247,32 @@ struct ContentView: View {
         }
         
         else if(showCount == 2){
-            ARViewContainer().edgesIgnoringSafeArea(.all)
+            ZStack{
+                ARViewContainer(sceneName: $showCount).edgesIgnoringSafeArea(.all)
+                VStack{
+                    Spacer()
+                    Spacer()
+                    Spacer()
+                    Spacer()
+                    Button("Go Home"){
+                        showCount = -1
+                    }
+                    .padding()
+                    .foregroundColor(.white)
+                    .background(Color.blue)
+                    .cornerRadius(10)
+                    Spacer()
+                }
+            }
+        }
+        else if(showCount == -1){
+            ZStack{
+                ARViewContainer(sceneName: $showCount).edgesIgnoringSafeArea(.all)
+                HStack{
+                    Text("Please Scan Icon")
+                        .foregroundColor(.primary)
+                }
+            }
         }
     }
 }
@@ -211,8 +281,8 @@ func AddOrder(pressedButtons: [String], message: String) -> String{
     
     var temp = message
     
-    for orderText in pressedButtons{
-        temp += " " + orderText
+    for button in pressedButtons{
+            temp += "\n" + button
     }
     
     return temp
@@ -220,22 +290,101 @@ func AddOrder(pressedButtons: [String], message: String) -> String{
 
 struct ARViewContainer: UIViewRepresentable {
     
+    @Binding var sceneName: Int
+    
     func makeUIView(context: Context) -> ARView {
         
-        let arView = ARView(frame: .zero)
+        if(sceneName == 2){
+            let arView = ARView(frame: .zero)
+            let config = ARWorldTrackingConfiguration()
+
+            config.planeDetection = .horizontal
+            
+            
+            guard let referenceImages = ARReferenceImage.referenceImages(inGroupNamed: "AR Resources", bundle: nil) else {
+                fatalError("Missing expected asset catalog resources.")
+            }
+            
+            config.detectionImages = referenceImages
+            
+            arView.session.run(config)
+            
+            context.coordinator.view = arView
+            
+            arView.session.delegate = context.coordinator
+            
+            if (try? Experience.loadBox()) != nil {
+                // Load the "Box" scene from the "Experience" Reality File
+                let boxAnchor = try! Experience.loadBox()
+                
+                // Add the box anchor to the scene
+                arView.scene.anchors.append(boxAnchor)
+                context.coordinator.scene1 = boxAnchor
+            }
+            
+            return arView
+
+        }
         
-        // Load the "Box" scene from the "Experience" Reality File
-        let boxAnchor = try! Experience.loadBox()
-        
-        // Add the box anchor to the scene
-        arView.scene.anchors.append(boxAnchor)
-        
-        return arView
-        
+        else {
+            
+            let arView = ARView(frame: .zero)
+            
+            context.coordinator.view = arView
+            
+            // Load the "Box" scene from the "Experience" Reality File
+            let boxAnchor = try! Experience.loadScene()
+            
+            context.coordinator.scene2 = boxAnchor
+            
+            boxAnchor.actions.sceneStarted.onAction = context.coordinator.Start(_:)
+
+            // Add the box anchor to the scene
+            arView.scene.anchors.append(boxAnchor)
+            
+            return arView
+
+        }
+            
     }
     
     func updateUIView(_ uiView: ARView, context: Context) {}
     
+    func makeCoordinator() -> Coordinator {
+        Coordinator(showCount: $sceneName)
+    }
+}
+
+class Coordinator:NSObject, ARSessionDelegate
+{
+    //Storing values
+    @Binding var showCount: Int
+    weak var view:ARView?
+    weak var scene1: Experience.Box?
+    weak var scene2: Experience.Scene?
+
+    required override init() {
+        fatalError("init() has not been implemented")
+    }
+
+    init(showCount: Binding<Int>) {
+        self._showCount = showCount
+        super.init()
+    }
+    
+    func session(_ session: ARSession, didAdd anchors:[ARAnchor]){
+        for anchor in anchors{
+            if(anchor.name == "donut"){
+                let anchorEntity = AnchorEntity(world: anchor.transform)
+                anchorEntity.addChild(scene1!)
+                view!.scene.anchors.append(anchorEntity)
+            }
+        }
+    }
+    
+    func Start(_ entity: Entity?){
+        showCount = 0
+    }
 }
 
 struct MessageComposeView: UIViewControllerRepresentable {
@@ -271,11 +420,15 @@ struct MessageComposeView: UIViewControllerRepresentable {
 
         func messageComposeViewController(_ controller: MFMessageComposeViewController, didFinishWith result: MessageComposeResult) {
             
+            isShowingMessageComposer = false
+            print(result.rawValue)
             
-            if(result.rawValue == 1){
+            if(result.rawValue == 0){
+                showCount = 0
+            }
+            else if(result.rawValue == 1){
                 showCount = 1
             }
-            isShowingMessageComposer = false
         }
         
     }
